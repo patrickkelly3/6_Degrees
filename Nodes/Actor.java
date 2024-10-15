32a1c89ca9dbd11e5f866e8eaafb6f44a2c33717
patrickkelly3 <p3kellyga@gmail.com>
2024-10-15 12:18:40 -0400

    Created basic Nodes to store Actor and Movie information to be pushed onto pathway list

---
+++ Nodes/Actor.java
@@ -0,0 +1,32 @@
+package Nodes;
+public class Actor {
+    
+    private String id;
+    private String name;
+    private String genre;
+
+    public String getID() {
+        return this.id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getGenre() {
+        return this.genre;
+    }
+
+    public void setID(String id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+    
+}
---
+++ Nodes/Movie.java
@@ -0,0 +1,22 @@
+package Nodes;
+
+public class Movie {
+    private String id;
+    private String name;
+
+    public String getID() {
+        return this.id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setID(String id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
