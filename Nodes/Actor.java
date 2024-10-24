bb220f952ebfdfb5f017230bbc7ba9fd717c7755
patrickkelly3 <p3kellyga@gmail.com>
2024-10-24 08:21:43 -0400

    Added documentation for Actor and Movie class

--- Nodes/Actor.java
+++ Nodes/Actor.java
@@ -1,22 +1,30 @@
 package Nodes;
 public class Actor {
     
-    private String id;
-    private String name;
-    private String genre;
+    private String id; //ID number of the actor for SQL Query referencing
+    private String name; //Name of the Actor for user manipulation and understanding
+    
 
+    /**
+     * 
+     * @return String: ID number of the actor
+     */
     public String getID() {
         return this.id;
     }
 
+    /**
+     * 
+     * @return String: Name of the actor
+     */
     public String getName() {
         return this.name;
     }
 
-    public String getGenre() {
-        return this.genre;
-    }
-
+    /**
+     * 
+     * @param id
+     */
     public void setID(String id) {
         this.id = id;
     }
@@ -25,8 +33,6 @@ public class Actor {
         this.name = name;
     }
 
-    public void setGenre(String genre) {
-        this.genre = genre;
-    }
+   
     
 }
--- Nodes/Movie.java
+++ Nodes/Movie.java
@@ -1,22 +1,55 @@
 package Nodes;
 
 public class Movie {
-    private String id;
-    private String name;
-
+    
+    private String id; //Movie id for reference to the SQL Query
+    private String name; //Movie name for user manipulation and understanding
+    private String genre; //Movie genre for algorithm list reduction
+    /**
+     * 
+     * @return String: id number of the movie
+     */
     public String getID() {
         return this.id;
     }
 
+    /**
+     * 
+     * @return String: the name of the movie
+     */
     public String getName() {
         return this.name;
     }
 
+    /**
+     * 
+     * @return String: genre of the movie
+     */
+    public String getGenre() {
+        return this.genre;
+    }
+
+    /**
+     * 
+     * @param id : New id number to set Movie id variable to
+     */
     public void setID(String id) {
         this.id = id;
     }
 
+    /**
+     * 
+     * @param name : New name of movie to set the variable to
+     */
     public void setName(String name) {
         this.name = name;
+    }
+
+    /**
+     * 
+     * @param genre : New genre of movie to set the variable to
+     */
+    public void setGenre(String genre) {
+        this.genre = genre;
     }
 }
