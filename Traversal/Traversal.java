24e261251e52fb52f0e1d63bf5d6c1b74338b02d
patrickkelly3 <p3kellyga@gmail.com>
2024-11-05 14:47:05 -0500

    Attempt to begin traversal methods

--- Traversal/Traversal.java
+++ Traversal/Traversal.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 public class Traversal {
     
-    
+    private String actorID2;
     public Connection connectToDatabase() {
         try {
             Connection databaseConnection = DriverManager.getConnection("jdbc:mysql://localhost:3306/6_Degrees", "root", "Hunkerdown0809!");
@@ -17,8 +17,11 @@ public class Traversal {
     
     
     public ArrayList<String> traversing(String actorID1, String actorID2) {
+        
         Connection databaseConnection = connectToDatabase();
         Statement statement;
+        this.actorID2 = actorID2;
+        
         try {
             
             //databaseConnection = DriverManager.getConnection(URL, username, password);
@@ -56,14 +59,59 @@ public class Traversal {
         return set;
     }
 
-    private ArrayList<String> actorTraversalRecursion (ArrayList<String> path, String actorID, int count) {
+    private ArrayList<String> actorTraversalRecursion (ArrayList<String> path, String actorID, int count, Connection con) {
+        path.add(actorID);
+        if (count > 6)
+            return null;
+        else if (actorID.equals(this.actorID2))
+            return path;
 
+            try {
+                Statement statement = con.createStatement();
+                ResultSet set = getResultSet(actorID, "CAST", statement);
+                ArrayList<String> loopPath = new ArrayList();
+                while (set.next()) {
+                    if (!set.getString("movieId").equals(actorID))
+                        loopPath = movieTraversalRecursion(path, set.getString("movieId"), ++count, con);
+                    if (loopPath != null)
+                        break;
+                }
+    
+                path.addAll(loopPath);
+    
+            } 
+                catch (Exception e) {
+            }
+            
+            return path;
         
     }
 
-    private ArrayList<String> movieTraversalRecursion(ArrayList<String> path, String movieId, int count) {
-
+    private ArrayList<String> movieTraversalRecursion(ArrayList<String> path, String movieId, int count, Connection con) {
+        path.add(movieId);
+        if (count > 6)
+            return null;
+        
+            try {
+                Statement statement = con.createStatement();
+                ResultSet set = getResultSet(movieId, "CAST", statement);
+                ArrayList<String> loopPath = new ArrayList();
+                while (set.next()) {
+                    loopPath = movieTraversalRecursion(path, set.getString("movieId"), ++count, con);
+                    if (loopPath != null)
+                        break;
+                }
+    
+                path.addAll(loopPath);
+    
+            } 
+                catch (Exception e) {
+            }
+            
+            return path;
     }
+
+    
 
 
 
